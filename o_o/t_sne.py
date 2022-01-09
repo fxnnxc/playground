b39bbadb6296938598865dc1d9e3aa041613eedd
fxnnxc <beomjin26@naver.com>
2022-01-09 18:45:26 +0900

    2021.01.09 working on sne

--- o_o/t_sne.py
+++ o_o/t_sne.py
@@ -1,49 +1,53 @@
 import torch 
+import matplotlib.pyplot as plt 
+from torch.autograd import Variable
 
-def compute_kernel(k, X,Y, get_pairs=False):
+def compute_kernel(k, X,Y):
     # compute kernel for all pairs in X and Y 
     assert X.ndim==2 and  Y.ndim==2
     assert X.size(1) == Y.size(1)
-    v = 0 
-    if not get_pairs:    
-        for i in range(X.size(0)):
-            for j in range(Y.size(0)):
-                v += k(X[i,:], Y[i,:])
-        return v
-    else:
-        pairs = []
-        for i in range(X.size(0)):
-            pairs.append([])
-            for j in range(Y.size(0)):
-                pair_value = k(X[i,:], Y[i,:])
-                pairs[-1].append(pair_value)
-                v += pair_value
-            pairs[-1] = torch.stack(pairs[-1])
-        return v, torch.stack(pairs)
 
+    pairs = []
+    for i in range(X.size(0)):
+        pairs.append([])
+        for j in range(Y.size(0)):
+            pair_value = k(X[i,:], Y[j,:])
+            pairs[-1].append(pair_value)
+        pairs[-1] = torch.stack(pairs[-1])
+    return torch.stack(pairs)
 
-def rbf_kernel(x,y, sigma=1):
-    v = (x-y)**2 / (2 * sigma**2)
+def rbf_kernel(x,y, sigma=10):
+    v = ((x-y).norm(2))**2 / (2 * sigma**2)
     return torch.exp(v)
 
 def entropy_loss(P, Q):
     # compute the entropy
-    return 0
+    loss = P * P.log() -  P * Q.log()
+    return loss.sum()
 
 
-def sne(X, kernel, dim=2, iteration=10):
-    v, P = compute_kernel(kernel, X, X, get_pairs=True)    
+def sne(X, kernel, dim=2, iteration=5, learning_rate=0.1):
+    P = compute_kernel(kernel, X, X)   
+    P /= P.clone().detach().norm(2, dim=0)
     # optimize 
-    Y = torch.rand(X.size(0), dim)
+    Y = Variable(torch.rand(X.size(0), dim), requires_grad=True)
     for i in range(iteration):
-        v2, Q = compute_kernel(kernel, Y, Y, get_pairs=True)
+        Q = compute_kernel(kernel, Y, Y)
+        Q /= Q.clone().detach().norm(2, dim=0)
         loss = entropy_loss(P, Q)
-        
+        if loss <0:
+            break
+        loss.backward()
+        Y.data -= learning_rate * Y.grad.data
     return Y
     
 
+def plot_2d(Y):
+    plt.scatter(Y[:,0], Y[:,1])
+    
+
 if __name__ == "__main__":
-    X = torch.rand(5,2)
-    Y = torch.rand(7,2)
-    print(compute_kernel(rbf_kernel, X, Y,  get_pairs=False))
-    print(compute_kernel(rbf_kernel, X, Y,  get_pairs=True))
+    X = torch.rand(100,20)
+    X[:,2:20] = 0
+    Y = sne(X, rbf_kernel)
+    plot_2d(Y.detach().numpy())
